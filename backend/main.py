f6c62b05ccca8e5ffb57bd73f8b12a3bf0042b77
Rithika-Gurusamy <rithika7rithika@gmail.com>
2026-03-27 12:12:32 +0530

    fix CORS browser security rejection for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -52,10 +52,15 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=[
+        "https://academia-five-dun.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
